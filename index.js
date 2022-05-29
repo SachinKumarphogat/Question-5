7e3e1bbee7d3aae36dae26387a5166384cb89182
SachinKumarphogat <100225961+SachinKumarphogat@users.noreply.github.com>
2022-05-29 18:44:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+let day=1
+
+switch(day){
+    case 1 :
+        console.log('The day of week is Sunday');
+        break
+    case 2 :
+        console.log('The day of week is Monday');
+        break;
+     case 3 :
+        console.log('The day of week is Tuesday');
+         break;
+     case 4 :
+        console.log('The day of week is Wednesday');
+         break;
+     case 5 :
+        console.log('The day of week is Thursday');
+         break;
+     case 6 :
+        console.log('The day of week is Friday');
+         break;
+    case 7 :
+        console.log('The day of week is Saturday');
+        break;
+    default :
+    console.log('Invalid')  
+    break;                          
+}
